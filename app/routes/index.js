ff4ffef633b2f4d8177a01175c95d9a988280024
Rafael J. Rodriguez <rafase282@gmail.com>
2016-08-10 10:25:13 -0400

    Changes from root to /whoami
    Changes the route to match the specific rout required for testing.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(app) {
-  app.route('/')
+  app.route('/whoami')
     .get(function(req, res) {
       var ip = req.headers['x-forwarded-for'] || 
      req.connection.remoteAddress || 
