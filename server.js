e5c092303644fd4a2f5b42fa217d9bc763760b71
Rafael J. Rodriguez <rafase282@gmail.com>
2016-08-10 10:21:15 -0400

    Adds CORS handling

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var app        = express();                 // define our app using express
 var routes = require('./app/routes/index.js');
     
 var port = process.env.PORT || 8080;        // set our port
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
     
 // The format follows as, alias to use for real path, also allows permission to such path.
 //app.use('/api', express.static(process.cwd() + '/app/api'));
